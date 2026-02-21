319d2c0126a5298d6c957afc8b70ae740681f674
Tanmay Gupta <43621750+tanmayGypt@users.noreply.github.com>
2026-02-21 15:29:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,134 +1,83 @@
-// document.querySelector(".w").addEventListener("keypress",()=>{
-//     var audio=new Audio("./sounds/snare.mp3");
-//     audio.play();
+// Get all drum buttons
+var drumButtons = document.querySelectorAll(".drum");
+var size = drumButtons.length;
 
-// });
-
-var size = document.querySelectorAll("button").length;
-
-for (var i = 0; i < size; i++) {
-
-    document.querySelectorAll("button")[i].addEventListener("keypress", (event) => {
-
-        switch (event.key) {
-            case 'w':
-                var audio = new Audio("./sounds/snare.mp3");
-                audio.play();
-                var activebutton = document.querySelector("." + event.key);
-                activebutton.classList.add("pressed");
-                setTimeout(() => {
-                    activebutton.classList.remove("pressed");
-                }, 100);
-                break;
-            case 'a':
-                var audio = new Audio("./sounds/tom-1.mp3");
-                audio.play();
-                var activebutton = document.querySelector("." + event.key);
-                activebutton.classList.add("pressed");
-                setTimeout(() => {
-                    activebutton.classList.remove("pressed");
-                }, 100);
-                break;
-            case 's':
-                var audio = new Audio("./sounds/tom-2.mp3");
-                audio.play();
-                var activebutton = document.querySelector("." + event.key);
-                activebutton.classList.add("pressed");
-                setTimeout(() => {
-                    activebutton.classList.remove("pressed");
-                }, 100);
-                break;
-            case 'd':
-                var audio = new Audio("./sounds/kick-bass.mp3");
-                audio.play();
-                var activebutton = document.querySelector("." + event.key);
-                activebutton.classList.add("pressed");
-                setTimeout(() => {
-                    activebutton.classList.remove("pressed");
-                }, 100);
-                break;
-            case 'j':
-                var audio = new Audio("./sounds/tom-3.mp3");
-                audio.play();
-                var activebutton = document.querySelector("." + event.key);
-                activebutton.classList.add("pressed");
-                setTimeout(() => {
-                    activebutton.classList.remove("pressed");
-                }, 100);
-                break;
-            case 'k':
-                var audio = new Audio("./sounds/tom-4.mp3");
-                audio.play();
-                var activebutton = document.querySelector("." + event.key);
-                activebutton.classList.add("pressed");
-                setTimeout(() => {
-                    activebutton.classList.remove("pressed");
-                }, 100);
-                break;
-            case 'l':
-                var audio = new Audio("./sounds/crash.mp3");
-                audio.play();
-                var activebutton = document.querySelector("." + event.key);
-                activebutton.classList.add("pressed");
-                setTimeout(() => {
-                    activebutton.classList.remove("pressed");
-                }, 100);
-                break;
-
-        }
-
-
-    });
-
-
+// Function to play sound based on key
+function playSound(key) {
+    switch (key) {
+        case 'w':
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        default:
+            console.log("Wrong key pressed");
+    }
 }
 
-// for (var j = 0; j < size; j++) {
-//     var clicked = document.querySelectorAll("button")[j].innerHTML;
-//     document.querySelectorAll("button")[j].addEventListener("click", () => {
+// Function to add animation
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    if (activeButton) {
+        activeButton.classList.add("pressed");
+        setTimeout(function() {
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
+}
 
-//         switch (clicked) {
-//             case 'w':
-//                 var audio = new Audio("./sounds/snare.mp3");
-//                 audio.play();
-                
-//                 break;
-//             case 'a':
-//                 var audio = new Audio("./sounds/tom-1.mp3");
-//                 audio.play();
-                
-               
-//                 break;
-//             case 's':
-//                 var audio = new Audio("./sounds/tom-2.mp3");
-//                 audio.play();
-//                 break;
-//             case 'd':
-//                 var audio = new Audio("./sounds/kick-bass.mp3");
-//                 audio.play();
-               
-//                 break;
-//             case 'j':
-//                 var audio = new Audio("./sounds/tom-3.mp3");
-//                 audio.play();
-                
-//                 break;
-//             case 'k':
-//                 var audio = new Audio("./sounds/tom-4.mp3");
-//                 audio.play();
-               
-//                 break;
-//             case 'l':
-//                 var audio = new Audio("./sounds/crash.mp3");
-//                 audio.play();
-                
-//                 break;
+// KEYBOARD EVENT - Listen for key presses on the whole document
+document.addEventListener("keydown", function(event) {
+    playSound(event.key);
+    buttonAnimation(event.key);
+});
 
-//         }
+// CLICK/TOUCH EVENT - Listen for clicks/touches on each button
+for (var i = 0; i < size; i++) {
+    
+    // Mouse click event
+    drumButtons[i].addEventListener("click", function() {
+        var buttonInnerHTML = this.innerHTML;
+        playSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+    
+    // Touch event for mobile devices
+    drumButtons[i].addEventListener("touchstart", function(e) {
+        e.preventDefault(); // Prevent default touch behavior (like scrolling)
+        var buttonInnerHTML = this.innerHTML;
+        playSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+}
 
-
-//     });
-
-
-// }
-
+// Fix for the k button background image (typo in your CSS)
+// Add this to your CSS or update the existing rule:
+/*
+.k {
+    background-image: url('./images/tom3.png'); /* was background-color */
+}
+*/
